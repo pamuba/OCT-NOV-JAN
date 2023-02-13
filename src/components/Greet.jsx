65b2ebaf17fba9c432a9723a151ac8e12935f2b8
kavyajadav <kjadhav@stevens.edu>
2023-02-14 03:30:38 +0530

    first second

--- src/components/Greet.jsx
+++ src/components/Greet.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Greet(props) {
   return (
-    <div>
+    <>
         <div>Hello {props.name} aka {props.heroName}</div>
         <div>{props.children}</div>
-    </div>
+    </>
   )
 }
 
